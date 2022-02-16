7824a2df354a5f00bc44a61e90edb1b5eae2b8b1
bugeonkim <bugeonkim@cvnet.co.kr>
2022-02-16 17:03:12 +0900

    HelloController 추가 HelloControllerTest 추가

---
+++ src/main/java/org/cvnet/chapter1/Application.java
@@ -0,0 +1,11 @@
+package org.cvnet.chapter1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
