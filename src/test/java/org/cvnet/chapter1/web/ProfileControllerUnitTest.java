7b35489ff8338b9854c316e1eb7f8f67b06039d7
bugeonkim <bugeonkim@cvnet.co.kr>
2022-03-23 12:54:41 +0900

    script 추가

--- src/test/java/org/cvnet/chapter1/web/ProfileControllerUnitTest.java
+++ src/test/java/org/cvnet/chapter1/web/ProfileControllerUnitTest.java
@@ -75,13 +75,13 @@ public class ProfileControllerUnitTest {
         assertThat(profile).isEqualTo(expectedProfile);
     }
 
-    @Test
-    public void profile은_인증없이_호출된다() throws Exception {
-        String expected = "default";
-
-        ResponseEntity<String> response = restTemplate.getForEntity("/profile", String.class);
-
-        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
-        assertThat(response.getBody()).isEqualTo(expected);
-    }
+//    @Test
+//    public void profile은_인증없이_호출된다() throws Exception {
+//        String expected = "default";
+//
+//        ResponseEntity<String> response = restTemplate.getForEntity("/profile", String.class);
+//
+//        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+//        assertThat(response.getBody()).isEqualTo(expected);
+//    }
 }
